2323f3f3c6f049d958e298be45e5d2a55bac7bb4
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-01-08 17:31:04 +0200

    make build

---
+++ index.ca22ec9f.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");e.style.display="flex",e.style.justifyContent="center",e.style.alignItems="center",t.style.position="static";
+//# sourceMappingURL=index.ca22ec9f.js.map
